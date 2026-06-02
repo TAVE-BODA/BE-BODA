f6958b7215fffa7a3ad465bb39c2fc24d75ec349
yunji1014 <7787yunji@gmail.com>
2026-06-03 00:16:58 +0900

    feat/project setting

---
+++ src/main/java/com/codit/be_boda/BeBodaApplication.java
@@ -0,0 +1,13 @@
+package com.codit.be_boda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codit/be_boda/BeBodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codit.be_boda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
