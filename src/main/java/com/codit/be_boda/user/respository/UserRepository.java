9f8fef978aac45044af2809169bc39401003eb62
uoahns23 <uoow.23@gmail.com>
2026-06-17 18:46:45 +0900

    feat/ user 정보 저장

---
+++ src/main/java/com/codit/be_boda/user/respository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.codit.be_boda.user.respository;
+
+import com.codit.be_boda.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByKakaoId(Long kakaoId);
+}
+
+
