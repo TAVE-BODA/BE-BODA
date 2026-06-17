289fbb68589bfa2b2f7cc8554841069e190e59c0
uoahns23 <uoow.23@gmail.com>
2026-06-17 19:30:03 +0900

    feat/ gititnore작성 및 application.properties수정

--- src/main/java/com/codit/be_boda/BeBodaApplication.java
+++ src/main/java/com/codit/be_boda/BeBodaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeBodaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BeBodaApplication.class, args);
 	}
 
