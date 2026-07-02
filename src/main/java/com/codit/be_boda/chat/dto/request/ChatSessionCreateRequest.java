df1a6ae79120a015718ae992693ae947a27b7054
YJ-Y <75804468+YJ-Y@users.noreply.github.com>
2026-07-02 19:08:03 +0900

    docs/채팅 API Swagger 설명 추가

--- src/main/java/com/codit/be_boda/chat/dto/request/ChatSessionCreateRequest.java
+++ src/main/java/com/codit/be_boda/chat/dto/request/ChatSessionCreateRequest.java
@@ -3,7 +3,6 @@ package com.codit.be_boda.chat.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-// 채팅방 생성 시 프론트가 보내는 값
 @Getter
 @NoArgsConstructor
 public class ChatSessionCreateRequest {
